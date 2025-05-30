9444c14f83fdef69f41b152d38a6019cf84fdc88
CharithaMadhumal <charithamadhumal174@gmail.com>
2025-05-30 14:53:05 +0530

    setup backend,end-point & login pages

---
+++ frontend/src/App.jsx
@@ -0,0 +1,46 @@
+import React, { useEffect } from 'react'
+import Navbar from './components/Navbar'
+import HomePage from './pages/HomePage'
+import SignUpPage from './pages/SignUpPage'
+import LoginPage from './pages/LoginPage'
+import SettingsPage from './pages/SettingsPage'
+import ProfilePage from './pages/ProfilePage'
+
+import { Routes, Route } from 'react-router-dom'
+import { useAuthStore } from './store/useAuthStore'
+
+import {Loader} from "lucide-react" 
+
+const App = () => {
+
+  const {authUser,checkAuth} = useAuthStore()
+
+  useEffect(()=>{
+    checkAuth()
+  },[checkAuth]);
+
+  console.log(authUser);
+
+  if(true)return(
+
+    <div className='flex items-center justify-center h-screen'>
+      <Loader className="size-10 animate-spin" />
+    </div>
+  )
+
+  return (
+    <div>
+      <Navbar />
+
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/signup' element={<SignUpPage />} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/settings' element={<SettingsPage />} />
+        <Route path='/profile' element={<ProfilePage />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
