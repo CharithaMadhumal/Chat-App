2bce065d9783732559f48f0fe74fdd7cc2d36199
CharithaMadhumal <charithamadhumal174@gmail.com>
2025-06-01 12:02:08 +0530

    completed signup page

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,5 +1,9 @@
 import {create} from 'zustand';
 import { axiosInstance } from '../lib/axios';
+import { data } from 'react-router-dom';
+import toast from 'react-hot-toast';
+import { LogOut } from 'lucide-react';
+
 
 export const useAuthStore = create((set)=>({
     authUser: null,
@@ -20,5 +24,30 @@ export const useAuthStore = create((set)=>({
         }finally{
             set({isCheckingAuth: false});
         }
+    },
+
+    signup: async (data) =>{
+        set({ isSigningUp: true});
+        try {
+            const res =  await axiosInstance.post("/auth/signup", data);
+            set({authUser: res.data});
+            toast.success("Account created successfully");
+        } catch (error) {
+            toast.error(error.response.data.message);
+            
+        }finally{
+            set({isSigningUp: false});
+        }
+
+    },
+
+    logout: async ()=>{
+        try {
+            await axiosInstance.post("/auth/logout");
+            set({authUser: null});
+            toast.success("Logged out successfully");
+        } catch (error) {
+            toast.error(error.response.data.message);
+        }
     }
-}))
+}));
